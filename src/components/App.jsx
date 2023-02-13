7546b65d35aa53c9a31129aca74398e427f06fc1
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2023-02-13 15:08:50 +0200

    first commiy with chenges

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import ProfileMarkup from './Profile'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+
     </div>
   );
 };
+
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
