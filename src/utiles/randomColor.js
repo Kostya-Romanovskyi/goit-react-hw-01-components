136900f01541f688d9d066feab9020b26714ff78
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2023-02-15 13:06:17 +0200

    fixed func randomColor

--- src/utiles/randomColor.js
+++ src/utiles/randomColor.js
@@ -1,3 +1,6 @@
 export default function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
